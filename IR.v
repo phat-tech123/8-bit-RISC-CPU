e720a3036589935ebc728db5a3cb3eb51449a34a
phat-tech123 <caovinhphat1904@gmail.com>
2024-12-03 16:06:51 +0700

    IR

--- IR.v
+++ IR.v
@@ -5,9 +5,26 @@ module instructionRegister(
 	output reg [4:0] address
 );
 
+reg [2:0] counter;
+
+initial begin 
+	counter = 3'd0;
+end
+
 always@(posedge clk) begin
-	opcode <= data[7:5];
-	address <= data[4:0];
+	if(counter == 0) begin
+		opcode <= data[7:5];
+		address <= data[4:0];
+		case(data[7:5])
+			3'b010: counter <= 3'd5;
+			3'b011: counter <= 3'd5;
+			3'b100: counter <= 3'd5;
+			3'b101: counter <= 3'd5;
+			3'b110: counter <= 3'd4;
+			default: counter <= 3'd3;
+		endcase
+	end else
+		counter <= counter - 1;
 end
 
 endmodule
