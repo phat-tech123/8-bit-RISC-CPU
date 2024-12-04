0efedb59f64b9fb56f6a5fdf02da920e69ffa0f5
phat-tech123 <caovinhphat1904@gmail.com>
2024-12-04 22:19:48 +0700

    upadting

--- controller.v
+++ controller.v
@@ -13,66 +13,67 @@ reg [2:0] counter;
 
 always@(posedge clk) begin
 	if(counter == 3'b0) begin
-	case(opcode)
-		3'b000:
-			write_en <= 1'b0;
-			skip <= 1'b1;
-			regWrite <= 1'b0;
-			ALUToACC <= 1'b0;
-			branch <= 1'b0;
-			ALU_Op <= 2'b00;
-		3'b001:
-			write_en <= 1'b0;
-			skip <= 1'b1;
-			regWrite <= 1'b0;
-			ALUToACC <= 1'b0;
-			branch <= 1'b0;
-			ALU_Op <= 2'b00;
-		3'b010:
-			write_en <= 1'b0; 
-			skip <= 1'b0;
-			regWrite <= 1'b1;
-			ALUToACC <= 1'b1;
-			branch <= 1'b0;
-			ALU_Op <= 2'b01;
-		3'b011:
-			write_en <= 1'b0; 
-			skip <= 1'b0;
-			regWrite <= 1'b1;
-			ALUToACC <= 1'b1;
-			branch <= 1'b0;
-			ALU_Op <= 2'b10;
-		3'b100:
-			write_en <= 1'b0; 
-			skip <= 1'b0;
-			regWrite <= 1'b1;
-			ALUToACC <= 1'b1;
-			branch <= 1'b0;
-			ALU_Op <= 2'b11;
-		3'b101:
-			write_en <= 1'b0;
-			skip <= 1'b0;
-			regWrite <= 1'b1;
-			ALUToACC <= 1'b0; 
-			branch <= 1'b0;
-			ALU_Op <= 2'b00;
-		3'b110:
-			write_en <= 1'b1; 
-			skip <= 1'b0;
-			regWrite <= 1'b0;
-			ALUToACC <= 1'b0;
-			branch <= 1'b0;
-			ALU_Op <= 2'b00;
-		3'b111:
-			write_en <= 1'b0; 
-			skip <= 1'b0;
-			regWrite <= 1'b0;
-			ALUToACC <= 1'b0;
-			branch <= 1'b1;
-			ALU_Op <= 2'b00;
+		case(opcode)
+			3'b000:
+				write_en <= 1'b0;
+				skip <= 1'b1;
+				regWrite <= 1'b0;
+				ALUToACC <= 1'b0;
+				branch <= 1'b0;
+				ALU_Op <= 2'b00;
+			3'b001:
+				write_en <= 1'b0;
+				skip <= 1'b1;
+				regWrite <= 1'b0;
+				ALUToACC <= 1'b0;
+				branch <= 1'b0;
+				ALU_Op <= 2'b00;
+			3'b010:
+				write_en <= 1'b0; 
+				skip <= 1'b0;
+				regWrite <= 1'b1;
+				ALUToACC <= 1'b1;
+				branch <= 1'b0;
+				ALU_Op <= 2'b01;
+			3'b011:
+				write_en <= 1'b0; 
+				skip <= 1'b0;
+				regWrite <= 1'b1;
+				ALUToACC <= 1'b1;
+				branch <= 1'b0;
+				ALU_Op <= 2'b10;
+			3'b100:
+				write_en <= 1'b0; 
+				skip <= 1'b0;
+				regWrite <= 1'b1;
+				ALUToACC <= 1'b1;
+				branch <= 1'b0;
+				ALU_Op <= 2'b11;
+			3'b101:
+				write_en <= 1'b0;
+				skip <= 1'b0;
+				regWrite <= 1'b1;
+				ALUToACC <= 1'b0; 
+				branch <= 1'b0;
+				ALU_Op <= 2'b00;
+			3'b110:
+				write_en <= 1'b1; 
+				skip <= 1'b0;
+				regWrite <= 1'b0;
+				ALUToACC <= 1'b0;
+				branch <= 1'b0;
+				ALU_Op <= 2'b00;
+			3'b111:
+				write_en <= 1'b0; 
+				skip <= 1'b0;
+				regWrite <= 1'b0;
+				ALUToACC <= 1'b0;
+				branch <= 1'b1;
+				ALU_Op <= 2'b00;
+		endcase
+	end else begin
+		counter <= counter - 1;
 	end
-end else begin
-	counter <= counter - 1;
 end
 	
 endmodule;
